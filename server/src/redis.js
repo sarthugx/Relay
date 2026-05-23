1c5efab6af16379f98d7f09da841bf89ebd86d5b
sarthug <smpingale2469@gmail.com>
2026-05-24 03:39:40 +0530

    redis.js fix for depl

--- server/src/redis.js
+++ server/src/redis.js
@@ -1,12 +1,24 @@
 const { createClient } = require('redis');
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:16379';
+const REDIS_URL = process.env.REDIS_URL;
 
-const pubClient = createClient({ url: REDIS_URL });
+const pubClient = createClient({
+  url: REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false
+  }
+});
+
 const subClient = pubClient.duplicate();
 
-pubClient.on('error', (err) => console.log('Redis Pub Client Error', err));
-subClient.on('error', (err) => console.log('Redis Sub Client Error', err));
+pubClient.on('error', (err) =>
+  console.log('Redis Pub Client Error', err)
+);
+
+subClient.on('error', (err) =>
+  console.log('Redis Sub Client Error', err)
+);
 
 module.exports = {
   pubClient,
